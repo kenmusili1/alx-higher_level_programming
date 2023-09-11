672d21ec1e296be86819b3b719d6aa4c115f2c24
kenmusili1 <kennedymusili99@gmail.com>
2023-09-11 21:56:25 +0000

    Python list

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints Python lists
+ * @p: Python Object
+ *
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+    Py_ssize_t size, i;
+    PyObject *item;
+
+    size = PyList_Size(p);
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
