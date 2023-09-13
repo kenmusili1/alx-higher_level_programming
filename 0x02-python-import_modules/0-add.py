10854d54bf1476448586c25969a2b18bdfcf73e7
kenmusili1 <kennedymusili99@gmail.com>
2023-09-13 22:19:41 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+def assgn():
 a = 1
 b = 2
 
 from add_0 import add
+print("{} + {} = {}".format(a, b, add(a, b)))
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    assgn()
