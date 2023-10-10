7d05f3b126276225ee6730786a4954ff3cc8110e
kenmusili1 <kennedymusili99@gmail.com>
2023-10-10 22:57:00 +0000

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains function that writes to text file and returns num chars written
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return(f.write(text))
