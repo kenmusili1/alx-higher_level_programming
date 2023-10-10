568a34efd305f7a6d323be8e3352cf4bf157af18
kenmusili1 <kennedymusili99@gmail.com>
2023-10-10 23:06:59 +0000

    append write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,10 @@ Contains function that writes to text file and returns num chars written
 """
 
 
-def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8)"""
-    with open(filename, mode="w", encoding="utf-8") as f:
-        return(f.write(text))
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file (UTF8)
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(text)
+    return len(text)
