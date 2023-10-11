f7373be415ed240042badd22498e6aef541b0024
kenmusili1 <kennedymusili99@gmail.com>
2023-10-11 00:09:35 +0000

    student 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+represents function
+that initializes public instance attributes first_name, last_name, and age,
+and has public method to_json that returns dictionary representation
+of requested attributes or all if none were requested
+"""
+
+
+class Student():
+    """
+        A Student class that defines a Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student with full name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns dictionary description with simple data structure
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    result[att] = self.__dict__[att]
+            return result
